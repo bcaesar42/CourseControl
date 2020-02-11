35c21714249664a8b3c37e4a32a2ff2700c3537b
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2020-02-10 16:16:57 -0800

    Added method to ITargetable interface.

--- Course Control/src/Target/ITargetable.cs
+++ Course Control/src/Target/ITargetable.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseControl.src.Action;
 
 namespace CourseControl.src.Target
 {
     interface ITargetable
     {
-
+        void HandleAction(Action action);
     }
 }
