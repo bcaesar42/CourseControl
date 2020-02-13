dd306ea14f8dcc91e8d9edfccf742c1bc7b12c8f
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2020-02-12 16:19:52 -0800

    Added rough version of TurnManager.

---
+++ Course Control/Assets/src/Turn/TurnManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Turn
+{
+    public class TurnManager
+    {
+        // Properties and fields:
+        private IEnumerable<Action.Action> Subscribers { get; set; }
+        public int TurnNum { get; private set; }
+
+
+        // Constructor:
+        public TurnManager()
+        {
+            Subscribers = new List<Action.Action>();
+        }
+
+
+        // Methods:
+        public bool SubscribeAction(Action.Action action) // The first "Action" specifies the namespace, the second "Action" specifies the class.
+        {
+            bool didSubscribe = false;
+
+            if (action != null && !Subscribers.Contains(action))
+            {
+                Subscribers.Append(action);
+            }
+
+            return didSubscribe;
+        }
+    }
+}
