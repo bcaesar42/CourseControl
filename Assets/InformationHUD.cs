69f930489f373315c79f9bf301ce8f11e36819f4
Jacob Berger <jberger.1919@gmail.com>
2020-03-02 15:26:25 -0800

    Auto stash before rebase of "origin/master"

---
+++ Assets/InformationHUD.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationHUD : MonoBehaviour
+{
+    int PlayerHealth { get; set; }
+    int EnemyHealth { get; set; }
+    int CrewTotal { get; set; }
+    int CrewRemaining { get; set; }
+    int Resources { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
