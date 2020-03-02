fb1ac7df5ca110c143a1b4dcbea8023ce09f74b7
ericshervey <ericbshervey@gmail.com>
2020-03-02 15:08:24 -0800

    Commiting TargetManager and TurnManager Framework
    See the title

--- Course Control/Assets/src/Controller/TargetManager/TargetManager.cs
+++ Course Control/Assets/src/Controller/TargetManager/TargetManager.cs
@@ -1,7 +1,107 @@
+using src.Model.ModelFramework.Targetables;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace src.Controller.TargetManager
 {
     public class TargetManager
     {
-        
+        List<ITargetable> targetList = new List<ITargetable>();
+        GameObject scene;
+
+        public TargetManager() //Scene should be a game object that all other game objects in the scene are under.
+        {
+
+        }
+
+        /*
+                private void getTargetableChildren(Transform parent) 
+                {
+                    ITargetable[] t = parent.gameObject.GetComponents<ITargetable>();
+
+                    foreach (ITargetable target in t)
+                    {
+                        if (target.getSelfId() == id) ;
+                        {
+                            targetList.Add(target);
+                        }
+                    }
+
+
+                    if (parent.childCount > 0)
+                    {
+                        foreach (Transform child in parent) //Should recusivly call down all children grabbing targetables and adding them.
+                        {
+                            getTargetableChildren(child);
+                        }
+                    }
+                }
+
+                    private void addTargetableChildren(Guid id, Transform parent)
+                {
+                    ITargetable[] t = parent.gameObject.GetComponents<ITargetable>();
+
+                    foreach (ITargetable target in t)
+                    {
+                        if (target.getSelfId() == id) ;
+                        {
+                            targetList.Add(target);
+                        }
+                    }
+
+                    if (parent.childCount > 0)
+                    {
+                        foreach (Transform child in parent) 
+                        {
+                            addTargetableChildren(id, child);
+                        }
+                    }
+                }
+
+        */
+        public void RemoveTarget(Guid id)
+        {
+            foreach(ITargetable t in targetList)
+            {
+                if(t.GetSelfId() == id)
+                {
+                    targetList.Remove(t);
+                }
+            }
+        }
+
+        public void AddTarget(ITargetable target)
+        {
+            targetList.Add(target);
+        }
+
+        public List<ITargetable> getEnemyID(Guid id)
+        {
+            List<ITargetable> rList = new List<ITargetable>();
+
+            foreach(ITargetable t in targetList)
+            {
+                if(t.GetSelfId() == id)
+                {
+                    rList.Add(t);
+                }
+            }
+            return rList;
+        }
+
+        public List<ITargetable> getAlliedID(Guid id)
+        {
+            List<ITargetable> rList = new List<ITargetable>();
+
+            foreach (ITargetable t in targetList)
+            {
+                if (t.GetSelfId() != id)
+                {
+                    rList.Add(t);
+                }
+            }
+            return rList;
+        }
     }
 }
--- Course Control/Assets/src/Turn/TurnManager.cs
+++ Course Control/Assets/src/Turn/TurnManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace src.Turn
-{
+{/*
     public class TurnManager
     {
         // Properties and fields:
@@ -34,5 +34,5 @@ namespace src.Turn
         }
 
 
-    }
+    } */
 }
