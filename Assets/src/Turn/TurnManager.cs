bea2dcc968d59ef6518df6e85200fbd74f61b49c
Colton Belfils <cb6619@gmail.com>
2020-03-07 17:24:08 -0800

    Some of health bar done

--- Assets/src/Turn/TurnManager.cs
+++ Assets/src/Turn/TurnManager.cs
@@ -9,25 +9,25 @@ namespace src.Turn
         // Constructor:
         public TurnManager()
         {
-            SubscribedActions_VeryLow = new List<Action.Action>();
-            SubscribedActions_Low = new List<Action.Action>();
-            SubscribedActions_Moderate = new List<Action.Action>();
-            SubscribedActions_High = new List<Action.Action>();
-            SubscribedActions_VeryHigh = new List<Action.Action>();
+            SubscribedActions_VeryLow = new List<Action>();
+            SubscribedActions_Low = new List<Action>();
+            SubscribedActions_Moderate = new List<Action>();
+            SubscribedActions_High = new List<Action>();
+            SubscribedActions_VeryHigh = new List<Action>();
         }
 
         // Properties and fields:
-        private IEnumerable<Action.Action> SubscribedActions_VeryLow { get; }
-        private IEnumerable<Action.Action> SubscribedActions_Low { get; }
-        private IEnumerable<Action.Action> SubscribedActions_Moderate { get; }
-        private IEnumerable<Action.Action> SubscribedActions_High { get; }
-        private IEnumerable<Action.Action> SubscribedActions_VeryHigh { get; }
+        private IEnumerable<Action> SubscribedActions_VeryLow { get; }
+        private IEnumerable<Action> SubscribedActions_Low { get; }
+        private IEnumerable<Action> SubscribedActions_Moderate { get; }
+        private IEnumerable<Action> SubscribedActions_High { get; }
+        private IEnumerable<Action> SubscribedActions_VeryHigh { get; }
         public int TurnNum { get; private set; }
 
 
         // Methods:
         public bool
-            SubscribeAction(Action.Action action,
+            SubscribeAction(Action action,
                 ActionPriority priority) // The first "Action" specifies the namespace, the second "Action" specifies the class.
         {
             var didSubscribe = false;
---
+++ Assets/src/View/HealthBar/HealthBar.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace src.View.HealthBar
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private int _health;
+        private GameObject[] _healthCells;
+        private Transform _healthBarTransform;
+
+        private void Start()
+        {
+            _healthCells = new GameObject[50];
+            _healthBarTransform = GetComponent<Transform>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                GameObject cell = new GameObject();
+                RectTransform cellTransform = cell.AddComponent<RectTransform>();
+                cellTransform.SetParent(_healthBarTransform);
+                cellTransform.pivot = new Vector2(0, 1);
+                cellTransform.sizeDelta = new Vector2(200, 300);
+                cellTransform.localScale = new Vector3(1, 1, 1);
+
+                int xPos = 20;
+                cellTransform.position = new Vector3(((xPos + 10) * i), -20, 0);
+
+                _healthCells[i] = cell;
+                SVGImage imageComponent = cell.AddComponent<SVGImage>();
+                imageComponent.color = Color.green;
+                cell.name = "cell" + i;
+                cell.SetActive(true);
+            }
+        }
+
+        private void Update()
+        {
+        }
+
+        public void StateChanged(int newHealth)
+        {
+            if (newHealth > 50)
+            {
+                _health = 50;
+            }
+            else if (newHealth < 0)
+            {
+                _health = 0;
+            }
+            else
+            {
+                _health = newHealth;
+            }
+        }
+    }
+}
