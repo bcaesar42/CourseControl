7bb0fa58f39d67307365af490227f1148401973c
ericshervey <ericbshervey@gmail.com>
2020-03-09 13:14:05 -0700

    EndTurn Button
    Added basic funciton for End Turn Button

--- Assets/src/Model/ModelConcrete/UI/EndTurnButton.cs
+++ Assets/src/Model/ModelConcrete/UI/EndTurnButton.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndTurnButton : MonoBehaviour
 {
     RealTurnManager rtm;
+    Button endTurnButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rtm = GameObject.Find("SceneManager").transform.GetComponent<RealTurnManager>();
+        endTurnButton = endTurnButton.GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TaskOnClick()
+    {
+        rtm.newTurn();
     }
 }
