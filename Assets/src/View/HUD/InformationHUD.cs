a49e83c232261884f4cba534113c880ce016a0c1
Jacob Berger <jberger.1919@gmail.com>
2020-03-09 14:24:03 -0700

    Created UI for ship and shield health.

--- Assets/src/View/HUD/InformationHUD.cs
+++ Assets/src/View/HUD/InformationHUD.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InformationHUD : MonoBehaviour
+namespace src.View.HUD
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InformationHUD : MonoBehaviour
     {
-        
-    }
+        private int ShipHealth { get; set; } = 0;
+        private int ShipTotal { get; set; } = 50;
+        private int ShieldHealth { get; set; } = 0;
+        private int ShieldTotal { get; set; } = 50;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Text ShipAndShieldHealth;
+
+        private void Start()
+        {
+            ShipAndShieldHealth = GameObject.Find("ShipAndShieldHealth").GetComponent<Text>();
+            // ShipAndShieldHealth.text = "Hull Integrity: 20 / 30\nShield Strength: 10 / 40";
+
+            /*Hull Integrity: 50/50
+            Shield Strength: 50/50*/
+        }
+
+        private void Update()
+        {
+            //For testing purposes
+            ShipHealth += 1;
+            ShieldHealth += 1;
+
+            ShipAndShieldHealth.text =
+                $"Hull Integrity: {ShipHealth} / {ShipTotal}\nShield Strength: {ShieldHealth} / {ShieldTotal}";
+            throw new NotImplementedException();
+        }
     }
 }
