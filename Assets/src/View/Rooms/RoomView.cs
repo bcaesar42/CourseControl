b9b03261ff21bceb19517b72c6a33e36789d6e85
Jacob Berger <jberger.1919@gmail.com>
2020-04-18 17:45:12 -0700

    Added remaining room models
    One room left missing from full ship

--- Assets/src/View/Rooms/RoomView.cs
+++ Assets/src/View/Rooms/RoomView.cs
@@ -30,6 +30,11 @@ public class RoomView : MonoBehaviour
         GameObject Player = GameObject.Find("Player");
         GameObject Room0, Room1, Room2, Room3, Room4, Room5, Room6, Room7 = null;
 
+        /*
+         * Current behavior is fixed. Rooms are manually placed and oriented because they all require a specific orientation/position. Will try to make modular closer to project completion.
+         * Translation can be accomplished in a two step manner after all rooms have been placed.
+         */
+
         try
         {
             text = GameObject.Find("Slot0CrewCount").GetComponent<TextMesh>();
@@ -37,7 +42,6 @@ public class RoomView : MonoBehaviour
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
-
             GameObject objPrefab = Resources.Load("WeaponRoom") as GameObject;
             GameObject obj = Instantiate(objPrefab, transform.position, Quaternion.identity) as GameObject;
             obj.transform.Rotate(new Vector3(-90, 0, -90));
@@ -55,34 +59,58 @@ public class RoomView : MonoBehaviour
             obj.transform.Rotate(new Vector3(0, 0, -135));
 
             text = GameObject.Find("Slot2CrewCount").GetComponent<TextMesh>();
-            room = GameObject.Find("Slot2").AddComponent<MedicalBay>() as MedicalBay;
+            room = GameObject.Find("Slot2").AddComponent<MaintenanceBay>() as MaintenanceBay;
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
+            objPrefab = Resources.Load("MaintenanceRoom") as GameObject;
+            obj = Instantiate(objPrefab, target, Quaternion.identity);
+            obj.transform.Rotate(new Vector3(0, 0, -90));
+            target = new Vector3(2.29f, -4.61f, -35.75f);
+            obj.transform.position = target;
 
             text = GameObject.Find("Slot3CrewCount").GetComponent<TextMesh>();
             room = GameObject.Find("Slot3").AddComponent<NavigationRoom>() as NavigationRoom;
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
+            objPrefab = Resources.Load("NavigationRoom") as GameObject;
+            obj = Instantiate(objPrefab, target, Quaternion.identity);
+            obj.transform.Rotate(new Vector3(-90, 0, 0));
+            target = new Vector3(-2.1f, -6.86f, -30.92f);
+            obj.transform.position = target;
 
             text = GameObject.Find("Slot4CrewCount").GetComponent<TextMesh>();
-            room = GameObject.Find("Slot4").AddComponent<ReplicationCenter>() as ReplicationCenter;
+            room = GameObject.Find("Slot4").AddComponent<ResearchCenter>() as ResearchCenter;
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
+            objPrefab = Resources.Load("ResearchRoom") as GameObject;
+            obj = Instantiate(objPrefab, target, Quaternion.identity);
+            obj.transform.Rotate(new Vector3(-90, 0, 0));
+            target = new Vector3(-2.15f, -6.86f, -22.91f);
+            obj.transform.position = target;
 
             text = GameObject.Find("Slot5CrewCount").GetComponent<TextMesh>();
-            room = GameObject.Find("Slot5").AddComponent<ResearchCenter>() as ResearchCenter;
+            room = GameObject.Find("Slot5").AddComponent<ScavengeBay>() as ScavengeBay;
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
+            objPrefab = Resources.Load("ScavengeRoom") as GameObject;
+            obj = Instantiate(objPrefab, target, Quaternion.identity);
+            target = new Vector3(-8.05f, -4.89f, -40.3f);
+            obj.transform.position = target;
 
             text = GameObject.Find("Slot6CrewCount").GetComponent<TextMesh>();
-            room = GameObject.Find("Slot6").AddComponent<SensorRoom>() as SensorRoom;
+            room = GameObject.Find("Slot6").AddComponent<ShieldBay>() as ShieldBay;
             Labels.Add(text); //Necessary?
             Rooms.Add(room); //Necessary?
             RoomMap.Add(room, text);
+            objPrefab = Resources.Load("ShieldRoom") as GameObject;
+            obj = Instantiate(objPrefab, target, Quaternion.identity);
+            obj.transform.Rotate(new Vector3(-90, 0, 0));
+            target = new Vector3(-12.14f, -6.88f, -48.88f);
+            obj.transform.position = target;
 
             text = GameObject.Find("Slot7CrewCount").GetComponent<TextMesh>();
             room = GameObject.Find("Slot7").AddComponent<ShieldBay>() as ShieldBay;
