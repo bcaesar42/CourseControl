3d8398ca6b27dd300899cc1b868d17356692e5e7
Jacob Berger <jberger8@eagles.ewu.edu>
2020-04-27 12:00:45 -0700

    Began initial work

---
+++ Assets/src/View/UI/CrewUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CrewUI : MonoBehaviour
+{
+    public TextMeshProUGUI crewCounts;
+    public List<GameObject> targets;
+    private int unallocated;
+    private int total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        unallocated = 10;
+        total = 10;
+        crewCounts.text = "Unallocated Crew: " + unallocated;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCounts();
+    }
+
+    private void UpdateCounts()
+    {
+        throw new NotImplementedException();
+    }
+}
