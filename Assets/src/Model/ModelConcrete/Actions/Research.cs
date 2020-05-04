9a3221f46b9dbc9b00eff357e9d0f7d0f2ba6cf8
Jacob Berger <jberger8@eagles.ewu.edu>
2020-05-04 11:39:25 -0700

    Quickly implemented Research action

--- Assets/src/Model/ModelConcrete/Actions/Research.cs
+++ Assets/src/Model/ModelConcrete/Actions/Research.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using src.Controller.TargetManager;
 using src.Model.ModelFramework.ActionFramework;
 using src.Model.ModelFramework.Targetables;
@@ -9,9 +10,14 @@ namespace src.Model.ModelConcrete.Actions
 {
     public class Research : Action
     {
+        private int _ResearchTokens { get; set; } = 0;
+        public int ResearchTokens { get => _ResearchTokens; }
+        private TargetManager _TargetManager;
+
         public Research(TargetManager targetManager, Guid actionId, Guid actionInstanceId, Guid selfId, Guid teamId) :
             base(targetManager, actionId, actionInstanceId, selfId, teamId)
         {
+            _TargetManager = targetManager;
         }
 
         public override ActionModel GetActionModel()
@@ -21,12 +27,16 @@ namespace src.Model.ModelConcrete.Actions
 
         public override IEnumerable<ITargetable> AvailableTargets()
         {
-            throw new NotImplementedException();
+            yield return _TargetManager.getAlliedID(SelfId).FirstOrDefault();
         }
 
         protected override void DoAction(int roundNum, IEnumerable<ITargetable> targets)
         {
-            throw new NotImplementedException();
+            if (ActionUsable)
+            {
+                _ResearchTokens++;
+                ActionUsable = false;
+            }
         }
     }
 }
