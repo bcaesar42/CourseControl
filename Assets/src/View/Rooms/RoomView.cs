9d9ba37ab0da7b72aaf3d27ba930027fff141a12
Jacob Berger <jberger8@eagles.ewu.edu>
2020-05-16 21:37:14 -0700

    Fixed label updating
    Variables can be reduced.

--- Assets/src/View/Rooms/RoomView.cs
+++ Assets/src/View/Rooms/RoomView.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using src.Model.ModelFramework.ActionFramework;
 using src.View.Rooms;
 using src.View.Rooms.ConcreteRooms;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 
 public class RoomView : MonoBehaviour
 {
@@ -17,6 +14,8 @@ public class RoomView : MonoBehaviour
     string RoomModelPath { get; set; }
     int CrewCount { get; set; }
     string[] selectedRooms { get; set; }
+    BaseRoom room { get; set; }
+    List<TextMesh> Labels { get; set; }
     //ActionState State;
 
     // Start is called before the first frame update
@@ -27,11 +26,10 @@ public class RoomView : MonoBehaviour
         {
              selectedRooms = new string[] { "WeaponsBay", "DroneBay", "MaintenanceBay", "NavigationRoom", "ResearchCenter", "ScavengeBay", "ShieldBay", "SensorRoom" };
         }
-        TextMesh text;
-        BaseRoom room;
+        TextMesh text = new TextMesh();
+        Labels = new List<TextMesh>();
 
         GameObject Player = GameObject.Find("Player");
-        GameObject Room0, Room1, Room2, Room3, Room4, Room5, Room6, Room7;
 
         Vector3[] Slots = { new Vector3(7.8639f, -4.3603f, -55.895f), new Vector3(6.2546f, -4.5828f, -46.731f), new Vector3(3.8639f, -6.8602f, -37.895f), new Vector3(-2.1361f, -6.8602f, -30.895f), new Vector3(-2.1361f, -4.8872f, -25.331f), new Vector3(-8.1361f, -6.8602f, -37.895f), new Vector3(-12.136f, -6.8602f, -48.895f), new Vector3(-14.2f, -6.3376f, -55.818f) };
 
@@ -39,6 +37,7 @@ public class RoomView : MonoBehaviour
          * Current behavior is fixed. Rooms are manually placed and oriented because they all require a specific orientation/position. Will try to make modular closer to project completion.
          * Translation can be accomplished in a two step manner after all rooms have been placed.
          * Will have to fix orientation in order to not rotate rooms.
+         * Global variables could be cleaned up but I can't figure out how currently.
          */
 
         try
@@ -89,6 +88,7 @@ public class RoomView : MonoBehaviour
                         break;
                 }
                 obj = Instantiate(objPrefab, transform.position, Quaternion.identity) as GameObject;
+                Labels.Add(text);
                 //obj.transform.Rotate(new Vector3(-90, 0, -90));
                 obj.transform.position = Slots[slotNumber];
                 RoomMap.Add(room, text);
@@ -104,14 +104,12 @@ public class RoomView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Will fix soon
         //Update room counts
-        //int ix = 0;
-        //foreach (BaseRoom room in Rooms)
-        //{
-        //    Labels[ix].text = "" + room.GetCrewCount();
-        //    ix++;
-        //}
+        for (int ix = 0; ix < Labels.Count; ix++)
+        {
+            Labels[ix].text = "" + room.GetCrewCount();
+            ix++;
+        }
     }
 
     void StateChanged(string description, string modelPath, int crewCount, ActionState state)
