af691f10829e832cda97f1744b29684a8f4293ba
Jacob Berger <jberger8@eagles.ewu.edu>
2020-06-04 16:41:27 -0700

    View accurately shows counts

--- Assets/src/View/Rooms/RoomView.cs
+++ Assets/src/View/Rooms/RoomView.cs
@@ -5,6 +5,7 @@ using src.Model.ModelFramework.ActionFramework;
 using src.View.Rooms;
 using src.View.Rooms.ConcreteRooms;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomView : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class RoomView : MonoBehaviour
     private List<TextMesh> Labels { get; set; }
     private ActionState State { get; set; }
     private int CrewCount { get; set; }
+    private List<BaseShip> shipList { get; set; }
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +30,11 @@ public class RoomView : MonoBehaviour
              selectedRooms = new string[] { "WeaponsBay", "DroneBay", "MaintenanceBay", "NavigationRoom", "ResearchCenter", "ScavengeBay", "ShieldBay", "SensorRoom" };
         }
         TextMesh text = new TextMesh();
-        Labels = new List<TextMesh>();
+        Labels = GameObject.Find("Player").GetComponentsInChildren<TextMesh>().ToList<TextMesh>();
 
         GameObject Player = GameObject.Find("Player");
         SceneManager sceneManager = GameObject.Find("SceneManager").transform.GetComponent<SceneManager>();
+        shipList = sceneManager.shipList;
 
         Vector3[] Slots = { new Vector3(7.8639f, -4.3603f, -55.895f), new Vector3(6.2546f, -4.5828f, -46.731f), new Vector3(3.8639f, -6.8602f, -37.895f), new Vector3(-2.1361f, -6.8602f, -30.895f), new Vector3(-2.1361f, -4.8872f, -25.331f), new Vector3(-8.1361f, -6.8602f, -37.895f), new Vector3(-12.136f, -6.8602f, -48.895f), new Vector3(-14.2f, -6.3376f, -55.818f) };
 
@@ -139,9 +142,11 @@ public class RoomView : MonoBehaviour
     void Update()
     {
         //Update room counts
-        for (int ix = 0; ix < Labels.Count; ix++)
+
+        BaseShip playerShip = shipList.First();
+        for (int ix = 0; ix < playerShip.roomList.Count; ix++)
         {
-            Labels[ix].text = $"{room.GetCrewCount()}\n{room.State}";
+            Labels[ix].text = $"{playerShip.roomList[ix].GetCrewCount()}";
         }
     }
 
